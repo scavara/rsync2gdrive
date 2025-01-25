5d36584a736094db5aee6a1a75e318d4544df2c4
Saša Čavara <scavara@gmail.com>
2025-01-25 15:31:10 +0100

    make process last enough to be cought by process metric sraper

--- home/scavara/.local/bin/sync2gdrive.sh
+++ home/scavara/.local/bin/sync2gdrive.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
+start_time=$(date +%s)
+
 TODAY=$(date +%Y/%m/%d)
 REMOTE_HOST=homeassistant.lan
 REMOTE_USER=root
 REMOTE_DIR=/media/usb-sda1/FTP
-#REMOTE_SUBDIRS="back-camera-1 backyard-camera-2 front-camera-1"
 REMOTE_SUBDIRS="back-camera-1 backyard-camera-2 front-camera-1"
 GDRIVE_DIR="Home Surveillance Recordings"
 LOCAL_DIR="/home/scavara/gdrive"
 RSYNC_CMD="/usr/bin/rsync"
-#RSYNC_ARGS="--compress --archive --update --debug=all5"
 RSYNC_ARGS="--compress --archive --update --quiet"
-
-while true
-do
 
 for subdir in $REMOTE_SUBDIRS
 do
@@ -25,5 +22,12 @@ do
 	fi
 	rsync -e "ssh -l root" $RSYNC_ARGS "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/$subdir/$TODAY/" "$LOCAL_DIR/$GDRIVE_DIR/$subdir/$TODAY/"
 done
-sleep 600
-done
+
+end_time=$(date +%s)
+runtime=$(echo "$end_time - $start_time" | bc)
+
+if [ $runtime -lt 10 ] # proces metric sraper frequency is 10s
+then
+sleep 10
+fi
+
